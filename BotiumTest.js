95ff34705beefbb6f653c03bfeb85a9373fa9d61
avinash.kalyanasundaram@paxport.net <Auguaugu@1>
2018-05-10 17:12:56 +0530

    first commit

---
+++ BotiumTest.js
@@ -0,0 +1,34 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+const fs = require('fs')
+
+const BotDriver = require('botium-core').BotDriver
+const Capabilities = require('botium-core').Capabilities
+const Source = require('botium-core').Source
+
+const driver = new BotDriver()
+	
+const compiler = driver.BuildCompiler()
+compiler.ReadScriptsFromDirectory('convos')
+	
+//console.log(compiler.convos)
+compiler.ExpandConvos()
+compiler.convos.forEach((convo) => console.log(convo.toString()))
+
+driver.BuildFluent()
+  .Start()
+  .ReadScripts('convos')
+  .RunScripts()
+  .Exec()
+  .then(() => {
+	console.log('')
+    console.log('SUCCESS: Had a successful Conversation')
+  })
+  .catch((err) => {
+	console.log('')
+	console.log('FAILURE')
+	console.log('')
+    console.log('ERROR: ', err)
+  })
