4ac4492ec876c815d5dc7aab83a4260abf1f1f7e
Ashok <ashokk@hexaware.com>
2018-07-04 12:12:00 +0530

    Changes done as part of code cleanup for folder reorganization

--- modules/Watson/ExcelReader.js
+++
@@ -1,7 +0,0 @@
-/**
- * Excel Reader Function*/
-
-if(typeof require !== 'undefined') XLSX = require('xlsx');
-var workbook = XLSX.readFile('D:\Book2.xlsx');
-var worksheet = workbook.Sheets['Dialogs'];
-console.log(XLSX.utils.sheet_to_json(worksheet,true));
